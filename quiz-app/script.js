279a496cc058a47140698f99ea402c84376c7c59
montaser-dev <abdallahelmontaser786@gmail.com>
2024-11-05 20:40:44 +0100

    Add Day 2 project: Quiz App and Day 3 project: Todo List App

---
+++ quiz-app/script.js
@@ -0,0 +1,43 @@
+const questions = [
+    {
+        question: "Which is larget animal in the world?",
+        answers: [
+            {text: "Shark", correct: false},
+            {text: "Blue whale", correct: true},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false},
+        ]
+    },
+
+    {
+        question: "Which is the smallest country in the world?",
+        answers: [
+            {text: "Vatican City", correct: true},
+            {text: "Bhutan", correct: false},
+            {text: "Nepal", correct: false},
+            {text: "Shri Lanka", correct: false},
+        ]
+    },
+    {
+        question: "Which is larget desert in the world?",
+        answers: [
+            {text: "Kalahari", correct: false},
+            {text: "Gobi", correct: false},
+            {text: "Sahara", correct: false},
+            {text: "Antarctica", correct: true},
+        ]
+    },
+    {
+        question: "Which is the smallest continent in the world?",
+        answers: [
+            {text: "Asia", correct: false},
+            {text: "Australia", correct: true},
+            {text: "Artic", correct: false},
+            {text: "Africa", correct: false},
+        ]
+    }
+];
+
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
